06d0fc13fcfbd48d95bc50fa546e38302e6cf8f9
haz3llongmeier <haz3l.long@gmail.com>
2023-07-13 13:57:59 -0700

    Initial Commit

---
+++ about me/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  about me
+//
+//  Created by scholar on 22/04/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack () {
+            Color(.systemCyan)
+                .ignoresSafeArea()
+            
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Text("About Me!")
+
+                Image("beachs")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .cornerRadius(15)
+                   
+                
+            
+                
+                       
+                Image("volleyball")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    
+             
+                HStack {
+                    Text("Hazel Longemeier!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+             
+                Text("Facts about me are that I play tennis and volleyball, I love to play piano, bake, draw, and code. I also love Starbucks and am a shopaholic!")
+                    .foregroundColor(Color(hue: 0.552, saturation: 0.533, brightness: 0.966))
+            }
+            
+            .padding()
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius:15)
+            .padding()
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ about me/about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_meApp.swift
+//  about me
+//
+//  Created by scholar on 22/04/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
